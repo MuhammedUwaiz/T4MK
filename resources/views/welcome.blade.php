c9fdd8fe816d5fd59078361ab4202934c23d5092
MuhammedUwaiz <uwais22222@gmail.com>
2024-06-15 17:17:04 +0530

    Normalize all the line endings

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,487 @@
+<!DOCTYPE html>
+<html lang="en" style="scroll-behavior: smooth;">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>t4mk.com</title>
+    <link rel="icon" type="image/x-icon" href="IMG_20231124_170848_845.jpg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+
+
+
+  <style>
+
+
+body {
+      margin: 0;
+      font-family: Arial, sans-serif;
+    }
+    .bg-hero-image {
+      background: url('/Images/hero-image.jpg') no-repeat center center;
+      background-size: cover;
+      height: 100vh;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .text-white {
+      color: white;
+    }
+    .text-center {
+      text-align: center;
+    }
+    .hero-content {
+        max-width: 1000px;
+    margin: auto;
+    background: rgba(0, 0, 0, 0.7);
+    padding: 20px;
+    border-radius: 10px;
+    }
+    .hero-content h1 {
+        font-weight: 600;
+        font-size: 3em;
+        margin-bottom: 0.5em;
+    }
+    .hero-content h2 {
+        font-weight: 600;
+      font-size: 2em;
+      margin-bottom: 1em;
+    }
+    .hero-content p {
+        font-weight: 600;
+      font-size: 1.2em;
+      margin-bottom: 2em;
+    }
+    .hero-content button {
+      background-color: #FFA500;
+      border: none;
+      padding: 0.75em 1.5em;
+      color: white;
+      font-size: 1em;
+      border-radius: 5px;
+      cursor: pointer;
+      font-weight: 600;
+    }
+    .hero-content button:hover {
+      background-color: #e59400;
+    }
+    
+    .content-section {
+    padding: 40px 20px;
+    background-color: #fff;
+    text-align: center;
+    font-weight: 400;
+   
+}
+
+.WHYT4MK{
+    text-align: center;
+    padding-bottom: 5rem;
+    font-weight: 500;
+}
+
+.header {
+    background-image: linear-gradient(to top, #dad4ec 0%, #dad4ec 1%, #f3e7e9 100%);
+    width: 100%;
+   
+}
+
+.content-section{
+    background-color: aliceblue;
+    width: 99%;
+}
+
+
+.footer {
+    background-color: #004080;
+    color: #fff;
+    padding: 20px 0;
+    text-align: center;
+    width: 99%;
+}
+
+.footer .cta {
+    font-size: 1.2em;
+    margin-top: 2em;
+}
+
+.footer .cta a {
+    color: #ffa500;
+    text-decoration: none;
+}
+
+.footer .mission-statement {
+    font-size: 1.5em;
+    margin-top: 2em;
+    font-weight: bold;
+}
+
+.contact{
+    font-weight: 800;
+   
+}
+
+.img-custom {
+        width: 180px;
+    }
+
+    #Experience-Section {
+    background-color: #f0f0f0 !important;
+    width: 99%;
+}
+
+.card {
+    text-align: center;
+    padding: 1rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+    width:auto;
+    height: 400px;
+}
+
+.card:hover {
+    transform: scale(1.05); 
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.card-img-top {
+    height: 15em;
+}
+
+.card-title {
+    font-weight: bold; 
+}
+
+
+@media only screen and (max-width: 400px) {
+
+    .card-text {
+        font-size: 12px;
+    }
+
+    .card-title {
+        font-size: 18px;
+    }
+
+    .card {
+    
+    padding: 0rem;
+    margin-left: -30px;
+    margin-top: 50px;
+    width: 119%;
+    
+}
+
+}
+
+
+@media only screen and (max-width: 767px) {
+
+    .card {
+   
+   margin-top: 50px;
+   
+}
+
+}
+
+
+
+@media only screen and (max-width: 990px) {
+
+    .img-custom {
+        width: 110px;
+        margin-left: -20px;
+    }
+
+    .bg-hero-image {
+        position: relative;
+        top: -15px;
+    }
+
+    .hero-content h1 {
+        font-weight: 500;
+        font-size: 2em;
+        
+    }
+    .hero-content h2 {
+        font-weight: 500;
+      font-size: 1.3em;
+    
+    }
+    .hero-content p {
+        font-weight: 500;
+      font-size: 0.8em;
+      
+    }
+    .hero-content button {
+      
+      
+      font-size: 0.8em;
+      border-radius: 2px;
+      font-weight: 500;
+    }
+
+    .footer .cta {
+    font-size: 0.8em;
+   
+}
+
+
+.footer .mission-statement {
+    font-size: 1em;
+   
+}
+
+.content-section{
+    width: 97.5%;
+}
+
+#Experience-Section {
+    width: 97.5%;
+}
+
+.footer {
+   
+    width: 97.5%;
+}
+
+
+
+   
+
+
+
+}
+
+
+    
+
+
+
+
+
+
+
+  </style>
+
+
+
+
+</head>
+
+
+<body> 
+    <header class="bg-white" style="width: 100%; position:fixed; z-index: 1;">
+        <div class="row header">
+            <div class="col-9 col-md-6 p-3 ps-5">
+                
+            <img src="\Images\T4MK-LOGO-ORIGINAL.png" class="img-fluid img-custom">
+
+
+            </div>
+
+            <div class="col-3 col-md-6 my-auto">
+                
+
+              <nav class="navbar navbar-expand-lg bg-aliceblue">
+                <div class="container-fluid">
+                    <button class="navbar-toggler navbar  ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+    <a class="nav-link active" style="color: black;" aria-current="page" href="#Home-Section">Home</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" style="color:  black;" href="#Experience-Section">Mission</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" style="color:  black;" href="#Contact me-Section">Contact</a>
+</li>
+
+                        </ul>
+                    </div>
+                </div>
+              </nav>
+
+            </div>
+        </div>
+    </header>
+
+    <section id="Home-Section" style="padding-top: 5em;">
+    <div class="row align-items-center bg-hero-image">
+      <div class="col text-center text-white hero-content">
+        <h1>Welcome to T4MK</h1>
+        <h2>Transforming Lives Through Technology</h2>
+        <p>At T4MK, we believe in the power of technology to make life easier and better for everyone. Our mission is to harness existing technologies and innovate new solutions that simplify daily tasks, enhance productivity, and improve the overall quality of life.</p>
+        <button>Join Our Mission</button>
+      </div>
+    </div>
+  </section>
+
+
+  <section id="mission" class="content-section">
+
+<div class="container">
+
+<h3>Join Us in Our Mission</h3>
+
+<p>
+Are you passionate about making a real difference through technology? Do you thrive in a dynamic, innovative environment? We are always on the lookout for talented individuals who share our vision. If you're ready to be part of something bigger, we want to hear from you!
+</p>
+
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+    <section class="bg-white p-5 " id="Experience-Section"  >
+     
+        <h1 class="WHYT4MK">Why T4MK?</h1>
+
+        
+<div class="row">
+
+
+  <div class="col-md-6">
+    <div class="card">
+      <img src="Images/Innovation-is-Just-Good-Product-Management-Blog-1-scaled.jpg"
+       class="card-img-top" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Innovate for Good</h4>
+        <p class="card-text">Work on meaningful projects that have a tangible impact on people's lives.</p>   
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="col-md-6">
+    <div class="card">
+      <img src="/Images/nm-2.jpg"
+       class="card-img-top" alt="" >
+      <div class="card-body">
+        <h4 class="card-title">Collaborative Culture</h4>
+        <p class="card-text">Join a team that values creativity, collaboration, and continuous learning.</p>     
+      </div>
+    </div>
+  </div>
+
+
+</div>
+
+
+
+<div class="row pt-5">
+
+
+
+
+<div class="col-md-6">
+    <div class="card">
+      <img src="Images/Combining-Cutting-Edge-Technologies-The-Power-of-AI-powered-Voice-Biometrics-and-Blockchain.png"
+       class="card-img-top" alt="" >
+      <div class="card-body">
+        <h4 class="card-title ">Cutting-Edge Technology</h4>
+        <p class="card-text">Utilize the latest tools and technologies in your work.</p>   
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="col-md-6">
+    <div class="card">
+      <img src="/Images/20190530201140-GettyImages-953171960.jpeg"
+       class="card-img-top" alt="" >
+      <div class="card-body">
+        <h4 class="card-title">Growth Opportunities</h4>
+        <p class="card-text">Enjoy professional development and career advancement in a supportive environment.</p>
+        
+      </div>
+    </div>
+  </div>
+
+
+
+</div>
+
+<div class="row pt-5">
+
+
+
+<div class="col-md-6 ">
+    <div class="card">
+      <img src="/Images/AdobeStock_442934125.jpeg"
+       class="card-img-top" alt="" >
+      <div class="card-body">
+        <h4 class="card-title ">Inclusive Workplace</h4>
+        <p class="card-text">Join our diverse, inclusive team where every voice is valued.</p>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+  <div class="col-md-6">
+    <div class="card">
+      <img src="/Images/What-makes-a-good-working-environment-for-researchers.png"
+       class="card-img-top" alt="" >
+      <div class="card-body">
+        <h4 class="card-title">Team Environment</h4>
+        <p class="card-text">Join a supportive community where teamwork and personal growth go hand in hand.</p>
+        
+      </div>
+    </div>
+  </div>
+
+
+
+  </div>
+
+
+
+
+
+    </section>
+      <section class="contact" id="Contact me-Section">
+<div class="container">
+  <h1 class="text-center ">Contact us</h1>
+  <div class="row">
+    <div class="col-md-12">
+
+    
+  </div>
+  </div>
+</div>
+      </section>
+
+      <footer class="footer" id="contact">
+        <div class="container">
+            <p class="cta">
+                <strong>Are You Ready to Change the World with Technology?</strong><br>
+                If you have a passion for harnessing technology to create impactful solutions, we want to meet you! 
+                Share your story with us by sending your resume and a cover letter detailing your experience and why you’re 
+                excited to join T4MK. Reach out to us at <a href="mailto:info@t4mk.com">info@t4mk.com</a>.
+            </p>
+            <p class="mission-statement">Join us in our mission to make life easier through technology!</p>
+        </div>
+    </footer>
+        
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</body>
+</html>
